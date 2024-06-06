687ba511cef01c93a9fe96e96cd5547ecb1107d2
Samgrass <samuelegrassetti01@gmail.com>
2024-06-06 18:28:50 +0200

    add variables for "box" text and background, change conditional statements to implement thosee variables

--- js/myscript.js
+++ js/myscript.js
@@ -2,30 +2,35 @@ const riga = document.querySelector(".row");
 
 // Scrivi un programma che stampi in console i numeri da 1 a 100
 for(let i = 1; i <= 100; i++) {
-    // creo una colonna con all'interno un div che rappresenti i numeri
-    let box =  `<div class="box bg-blue">
-                    <div>${i}</div>
-                </div>`; 
 
+    // creo variabile per contenuto
+    let txtBox = i;
+
+    // creo variabile per background
+    let bgClass;
+    
     // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.  
     if (i % 3 === 0 && i % 5 === 0) {
-        box = `<div class="box bg-red">
-                    <div>FizzBuzz</div>
-                 </div>`; 
-        
+        txtBox = "FizzBuzz";
+        bgClass = "bg-red";
     } 
      // per i multipli di 3 stampi “Fizz” al posto del numero 
     else if (i % 3 === 0) {
-        box = `<div class="box bg-green">
-                    <div>Fizz</div>
-                 </div>`;   
+        txtBox = "Fizz";
+        bgClass = "bg-green";  
     }  
     // per i multipli di 5 stampi “Buzz” al posto del numero 
     else if (i % 5 === 0) {
-        box = `<div class="box bg-yellow">
-                    <div>Buzz</div>
-                 </div>`; 
-    } else {}
+        txtBox = "Buzz";
+        bgClass = "bg-yellow";
+    } else {
+        bgClass = "bg-blue";
+    }
+
+    // creo una colonna con all'interno un div che rappresenti i numeri
+    let box =  `<div class="card ${bgClass}">
+                    <div>${txtBox}</div>
+                </div>`; 
 
     console.log(box);
 
