ac356d68d456b7d50876669195b16802b2d2fd3f
Samgrass <samuelegrassetti01@gmail.com>
2024-06-06 17:29:45 +0200

    add conditional statement to display different text in "box"

--- js/myscript.js
+++ js/myscript.js
@@ -3,21 +3,37 @@ const riga = document.querySelector(".row");
 // Scrivi un programma che stampi in console i numeri da 1 a 100
 for(let i = 1; i <= 100; i++) {
     // creo una colonna con all'interno un div che rappresenti i numeri
-    let box = `<div class="box">
+    let box =  `<div class="box">
                     <div>${i}</div>
-                 </div>`;
+                </div>`; 
+
+    // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.  
+    if (i % 3 === 0 && i % 5 === 0) {
+        box = `<div class="box">
+                    <div>FizzBuzz</div>
+                 </div>`; 
+        
+    } 
+    
+    else if (i % 3 === 0) {
+        box = `<div class="box">
+                    <div>Fizz</div>
+                 </div>`;   
+    }  
+    // per i multipli di 5 stampi “Buzz” al posto del numero 
+    else if (i % 5 === 0) {
+        box = `<div class="box">
+                    <div>Buzz</div>
+                 </div>`; 
+    } else {}
+
     console.log(box);
 
     riga.innerHTML += box;
-    // per i multipli di 3 stampi “Fizz” al posto del numero
+    
     
 
-    // per i multipli di 5 stampi “Buzz” al posto del numero 
-
-    // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-
-
-    
+      
 }
 
 
